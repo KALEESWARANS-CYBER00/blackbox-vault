86e2bfd9c25d6ab8b4bb798beb0c34a055b3c515
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-06-20 13:22:01 +0530

    Fixed all setup again

---
+++ src/main/java/com/blackbox/BlockboxApplication.java
@@ -0,0 +1,11 @@
+package com.blackbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockboxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlockboxApplication.class, args);
+    }
+}
---
+++ uploads/60dfa867-af1a-46ae-9c0d-53360cce14d7_backdoor.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unisrd.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <winuser.h>
+#include <wininet.h>
